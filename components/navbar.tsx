e887090d436ab64fbd186f9addd1fb3d8236a914
Brandon Quon <bqa7@sfu.ca>
2024-07-21 22:14:03 -0700

    Fixing window is not defined in the navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,18 @@
 "use client";
 import React from "react";
-import StickyHeadroom from "@integreat-app/react-sticky-headroom";
+import dynamic from "next/dynamic";
+
+// Dynamic import for StickyHeadroom with SSR disabled
+const StickyHeadroom = dynamic(
+  () => import("@integreat-app/react-sticky-headroom"),
+  { ssr: false },
+);
 
 export const Navbar = () => {
   const sections = ["Home", "About", "Projects", "Contact"];
 
   return (
-    <StickyHeadroom scrollHeight={100}>
+    <StickyHeadroom scrollHeight={100} pinStart={0}>
         <div className="w-full flex justify-center fixed top-[1vh] p-4 z-50">
         <div className="flex gap-8 justify-between px-4 py-2 h-full w-fit backdrop-filter backdrop-blur-md bg-[#232b2b]/60 rounded-full shadow-lg border border-gray-700/40">
             {sections.map((section, index) => (
