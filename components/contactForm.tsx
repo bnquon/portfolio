1b0ae5eac478c139c0c59b91a9e96c8f5e67eeb0
Brandon Quon <bqa7@sfu.ca>
2024-07-23 22:40:22 -0700

    Updated the contact form

--- components/contactForm.tsx
+++ components/contactForm.tsx
@@ -29,52 +29,54 @@ export const ContactForm = () => {
 
   return (
     <div className="sm:w-1/2 w-full self-end xl:mt-[16vh] sm:mt-[12vh] mt-[8vh]">
-      <div className="w-full flex flex-col gap-8 px-6 py-8 rounded-xl border-[1px] border-white">
-        <form
-          ref={formRef}
-          onSubmit={onSubmit}
-          className="w-full flex flex-col gap-8"
-        >
-          <div className="w-full flex xl:flex-row flex-col gap-8">
-            <div className="xl:w-1/2 w-full flex flex-col">
-              <p className="text-xl xl:text-2xl font-semibold text-white">Full Name</p>
-              <input
-                placeholder="John Doe"
-                name="fullName"
-                type="text"
-                required
-                className="w-full xl:px-2 xl:py-2 px-2 py-1 outline-none text-lg xl:text-xl font-medium rounded-lg"
-              />
-            </div>
-            <div className="xl:w-1/2 w-full flex flex-col">
-              <p className="text-xl xl:text-2xl font-semibold text-white">Email</p>
-              <input
-                placeholder="JohnDoe@example.com"
-                name="email"
-                type="email"
-                required
-                className="w-full xl:px-2 xl:py-2 px-2 py-1 outline-none text-lg xl:text-xl font-medium rounded-lg"
-              />
-            </div>
-          </div>
-
-          <div className="w-full flex flex-col">
-            <p className="xl:text-2xl text-xl font-semibold text-white">Message</p>
-            <textarea
-              placeholder="Enter your message here"
-              name="message"
-              required
-              rows={4}
-              className="w-full sm:px-2 sm:py-2 px-2 py-1 outline-none resize-none text-lg xl:text-xl font-medium rounded-lg"
-            />
-          </div>
-          <button
-            type="submit"
-            className="rounded-lg bg-white text-black sm:px-4 sm:py-2 px-3 py-2 w-fit text-xl xl:text-2xl font-medium hover:bg-black hover:text-white duration-150"
+      <div className="w-full flex flex-col gap-8 py-8 relative">
+        <article className="rounded-xl bg-gradient-to-r from-blue-400 to-[#E0B0FF] p-0.5">
+          <form
+            ref={formRef}
+            onSubmit={onSubmit}
+            className="w-full flex flex-col gap-8 z-10 bg-black rounded-[10px] p-8"
           >
-            Submit
-          </button>
-        </form>
+            <div className="w-full flex xl:flex-row flex-col gap-8">
+              <div className="xl:w-1/2 w-full flex flex-col">
+                <p className="text-xl xl:text-2xl font-semibold text-white">Full Name</p>
+                <input
+                  placeholder="John Doe"
+                  name="fullName"
+                  type="text"
+                  required
+                  className="w-full xl:px-2 xl:py-2 px-2 py-1 outline-none text-lg xl:text-xl font-medium rounded-lg"
+                />
+              </div>
+              <div className="xl:w-1/2 w-full flex flex-col">
+                <p className="text-xl xl:text-2xl font-semibold text-white">Email</p>
+                <input
+                  placeholder="JohnDoe@example.com"
+                  name="email"
+                  type="email"
+                  required
+                  className="w-full xl:px-2 xl:py-2 px-2 py-1 outline-none text-lg xl:text-xl font-medium rounded-lg"
+                />
+              </div>
+            </div>
+
+            <div className="w-full flex flex-col">
+              <p className="xl:text-2xl text-xl font-semibold text-white">Message</p>
+              <textarea
+                placeholder="Enter your message here"
+                name="message"
+                required
+                rows={4}
+                className="w-full sm:px-2 sm:py-2 px-2 py-1 outline-none resize-none text-lg xl:text-xl font-medium rounded-lg"
+              />
+            </div>
+            <button
+              type="submit"
+              className="rounded-xl bg-white text-black sm:px-4 sm:py-2 px-3 py-2 w-fit text-xl xl:text-2xl font-medium hover:bg-[#E0B0FF] hover:text-white duration-150"
+            >
+              Submit
+            </button>
+          </form>
+        </article>
       </div>
       <ToastContainer
         position="bottom-right"
