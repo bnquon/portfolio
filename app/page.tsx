16d29126b14abbdaa6ef935c4f6cdf530b918e1f
Brandon Quon <bqa7@sfu.ca>
2024-08-04 01:16:16 -0700

    Resume update

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function App() {
           <div className="inline-flex justify-center sm:gap-8 gap-4">
             <CTABtn />
             <a
-              href="https://drive.google.com/file/d/137AsgFU0Zt7CgW8-jinxi9i3pkKG1iPt/view?usp=sharing"
+              href="https://drive.google.com/file/d/1jNQ0nEBIQ8FecX1aTdCv_jMPldmItIVu/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
             >
